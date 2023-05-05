96dff6ec4405c448256e109a91d78ca604208d10
Eriwin <erwinis001@gmail.com>
2023-05-05 10:52:51 +0800

    add abstract class notify and its test.

---
+++ Notify.java
@@ -0,0 +1,25 @@
+package lib.system;
+import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+
+abstract public class Notify{
+     public void windowsNotify(String msg) throws AWTException {
+        //Obtain only one instance of the SystemTray object
+        SystemTray tray = SystemTray.getSystemTray();
+
+        //If the icon is a file
+        Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
+        //Alternative (if the icon is on the classpath):
+        //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
+
+        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
+        //Let the system resize the image if needed
+        trayIcon.setImageAutoSize(true);
+        //Set tooltip text for the tray icon
+        trayIcon.setToolTip("System tray icon demo");
+        tray.add(trayIcon);
+
+        trayIcon.displayMessage("Refrigerator Record", msg, MessageType.INFO);
+        tray.remove(trayIcon);
+    }
+} 
---
+++ NotifyTest.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+import lib.system.Notify;
+class NotifyTest{
+    public static void main(String[] args) throws AWTException {
+        if (SystemTray.isSupported()) {
+            Notify notify = new TestNotify();
+            notify.windowsNotify("你有一項食物快過期了");
+           
+        } else {
+            System.err.println("System tray not supported!");
+        }
+    }
+    static class TestNotify extends Notify{
+
+    }
+}
